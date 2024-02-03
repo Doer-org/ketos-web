ea159fcce5a4d2475eeb77eefd2cf2defaf1f8d7
miso-devel <kengo279@icloud.com>
2024-02-03 17:50:32 +0900

    sh cd test

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -2,17 +2,23 @@
 
 set -e
 
-ls
-
 # ensure required environment variables are defined
 if [ -z "$CF_ACCOUNT_ID" ] || [ -z "$CF_API_TOKEN" ] || [ -z "$CF_CUSTOM_DOMAIN" ] || [ -z "$CF_SCRIPT_NAME" ]; then
   echo "\$CF_ACCOUNT_ID, \$CF_API_TOKEN, \$CF_CUSTOM_DOMAIN, and \$CF_SCRIPT_NAME are required"
   exit 1
 fi
 
+cd ..
+
 ls_result=$(ls)
 echo "ls result"
 echo "$ls_result"
+
+cd server
+
+ls_result_a=$(ls)
+echo "ls result_a"
+echo "$ls_result_a"
 
 DENO_VERSION="v1.40.3"
 DENOFLARE_VERSION="v0.6.0"
