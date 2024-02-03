2ff3486d920dd8ec55ef514f4c3e76b379792ec1
miso-devel <kengo279@icloud.com>
2024-02-03 19:08:13 +0900

    APIを仮置きする

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,21 @@
-export default {
-  async fetch(request: Request): Promise<Response> {
-    return new Response("ハロー");
-  },
-};
+import { Hono } from "./deps.ts";
+
+const app = new Hono();
+app.fire();
+
+app.get("/health", (c) => c.json({ status: "ok" }));
+
+app.get("/:id", (c) => {
+  const id = c.req.param("id");
+  // TODO: DBから取ってくる
+  return c.json({ id });
+});
+
+app.post("/", async (c) => {
+  const input = await c.req.json();
+  const uuid = crypto.randomUUID();
+  console.log("input", input);
+  return c.json({ id: uuid });
+});
+
+export default app;
