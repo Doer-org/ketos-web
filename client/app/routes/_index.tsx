ae468a59ecb3305a591b47155d9ae4c8faf96118
miso-devel <kengo279@icloud.com>
2024-02-05 22:34:08 +0900

    HOMEまとめる

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,44 +1,24 @@
 import type { MetaFunction } from '@remix-run/cloudflare';
+import { Logo } from '~/components/Logo';
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'Ketos' },
     {
       name: 'description',
-      content: 'CLI for the fastest development environment for others',
+      content: `CLI tool to reproduce someone else's environment`,
     },
   ];
 };
 
 export default function Index() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Ketos</h1>
-      <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/blog'
-            rel='noreferrer'
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/jokes'
-            rel='noreferrer'
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className='flex flex-col justify-center items-center min-h-screen font-mono font-bold bg-teal-100'>
+      <Logo />
+      <h1 className='text-4xl font-bold'>Ketos</h1>
+      <p className='my-2'>
+        CLI tool to reproduce someone {"else's"} environment
+      </p>
+    </main>
   );
 }
