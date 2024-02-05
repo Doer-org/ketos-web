2d992e9ce4dfa8d68eff0b6027cf8951b9ab2947
miso-devel <kengo279@icloud.com>
2024-02-05 21:32:56 +0900

    metaを設定

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -2,15 +2,18 @@ import type { MetaFunction } from '@remix-run/cloudflare';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Ketos' },
+    {
+      name: 'description',
+      content: 'CLI for the fastest development environment for others',
+    },
   ];
 };
 
 export default function Index() {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className='text-3xl font-bold underline'>Ketos</h1>
       <ul>
         <li>
           <a
