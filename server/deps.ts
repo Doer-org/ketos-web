da3fa8afa8ae8349c700512a3fa2def50de1eb3b
miso-devel <kengo279@icloud.com>
2024-02-08 00:38:27 +0900

    deps整理

--- server/deps.ts
+++ server/deps.ts
@@ -1,2 +1,9 @@
 export { Hono, HTTPException } from "https://deno.land/x/hono@v3.12.5/mod.ts";
 export { expect } from "https://deno.land/std@0.214.0/expect/mod.ts";
+export * as fs from "https://deno.land/std@0.214.0/fs/mod.ts";
+export * as compress from "https://deno.land/x/compress@v0.4.6/zlib/mod.ts";
+export { unZipFromFile } from "https://deno.land/x/zip@v1.1.0/unzip.ts";
+export {
+  Tar,
+  type TarInfo,
+} from "https://deno.land/std@0.214.0/archive/tar.ts";
