4e06f5eeed97cbaae5541b485d7750a3eb4ffc1c
miso-devel <kengo279@icloud.com>
2024-02-09 01:05:15 +0900

    mainの処理を変える

--- server/main.ts
+++ server/main.ts
@@ -1,93 +1,52 @@
+import { getFile, uploadFile } from "./cloudStorage.ts";
 import { Hono, HTTPException } from "./deps.ts";
+import { toHashStr } from "./util.ts";
 
 export const app = new Hono();
 export const kv = await Deno.openKv();
 
-type TChunk = { buf: ArrayBuffer; order: number };
-type TKvIterChunks = Deno.KvListIterator<TChunk>;
+type FileInfo = {
+  id: string;
+  fileId: string;
+  port: string;
+};
 
 app.get("/health", (c) => c.json({ status: "ok" }));
 
 app.get("/:id", async (c) => {
   const id = c.req.param("id");
-  const { value: code } = await kv.get(["code", id]);
-  if (!code) throw new HTTPException(404, { message: "code is not found" });
-  const chunkIter: TKvIterChunks = kv.list({ prefix: ["chunk", id] });
-  const chunks: TChunk[] = [];
-  const chunkBytes: number[] = [];
-  for await (const chunk of chunkIter) {
-    chunkBytes.push(chunk.value.buf.byteLength);
-    chunks.push(chunk.value);
-  }
-  const chunkByteSum = chunkBytes.reduce((acc, cur) => acc + cur, 0);
-  const buf = new Uint8Array(chunkByteSum);
+  const { value: fileInfo } = await kv.get(["fileInfo", id]) as Deno.KvEntry<
+    FileInfo
+  >;
+  if (!fileInfo) throw new HTTPException(404, { message: "file is not found" });
+  const file = await getFile(fileInfo.fileId);
 
-  let current = 0;
-  let currentChunkSum = 0;
-  for (const chunk of chunks) {
-    buf.set(new Uint8Array(chunk.buf), currentChunkSum);
-    current++;
-    currentChunkSum += chunk.buf.byteLength;
-    console.log("currentChunSum", current, currentChunkSum, currentChunkSum);
-  }
-
-  if (!code) throw new HTTPException(404, { message: "chunk is not found" });
-  c.res.headers.set("content-type", "application/x-tar");
-
-  // プロセスが開始できないんだけど、多分.gitが入ってるからだと思う。
-  // あとこれでできなかったら const buffer =new Buffer から buffer.bytes()に変える
-  return c.body(buf.buffer);
+  return c.body(file);
 });
 
 // サーバー側で解凍してなんとかできないか模索する
 app.get("/file_info/:id", async (c) => {
   const id = c.req.param("id");
-  const { value } = await kv.get(["codes", id]);
-  Deno.writeFileSync("./temp.tgz", new Uint8Array(value as ArrayBuffer));
+  const { value: fileInfo } = await kv.get(["fileInfo", id]);
 
   return c.json({ message: "Hello, World!" });
 });
 
 app.post("/", async (c) => {
-  const { host } = c.req.query();
-  if (!host) throw new HTTPException(400, { message: "host is required" });
+  const { port } = c.req.query();
+  if (!port) throw new HTTPException(400, { message: "host is required" });
   const input = await c.req.parseBody();
   const file = input.upload_file as File;
-  const uuid = crypto.randomUUID();
-  const bufArray = await file.arrayBuffer();
-
-  // 約100万byte以上の場合はこのアプリケーションでは処理しない
-  const requiredChunkCount = Math.ceil(bufArray.byteLength / 30000);
-  // 33以下なら必要な数だけをchunkさせる。33回以上chunkが必要ならエラーを返す
-  const canDoChunk = requiredChunkCount < 50;
-  const bufSizeBychunk = Math.ceil(bufArray.byteLength / requiredChunkCount);
-
-  // console.log("バイト数:", bufArray.byteLength);
-  // console.log("必要なchunk回数:", requiredChunkCount);
-  // console.log("1回のchunkサイズ:", bufSizeBychunk);
-
-  if (!canDoChunk) {
-    throw new HTTPException(500, {
-      message: `${bufArray.byteLength - 1500000}byte分制限オーバーです`,
-    });
-  }
-  let currentBufPositon = 0;
-  let order = 1;
-
+  const id = crypto.randomUUID();
+  const fileId = await toHashStr(crypto.randomUUID());
   try {
-    await kv.set(["code", uuid], { id: uuid, host });
-    while (currentBufPositon < bufArray.byteLength) {
-      // console.log(`現在のチャンク:${currentBufPositon}`);
-      const buf = bufArray.slice(currentBufPositon, currentBufPositon + 30000);
-      await kv.set(["chunk", uuid, order], { buf, order });
-      currentBufPositon += bufSizeBychunk;
-      order++;
-    }
-  } catch (error) {
-    throw new HTTPException(500, { message: error });
+    await uploadFile(fileId, file);
+    await kv.set(["fileInfo", id], { id, fileId, port });
+  } catch (e) {
+    throw new Error("失敗:" + e);
   }
 
-  return c.json({ id: uuid, host });
+  return c.json({ id, fileId, port });
 });
 
 Deno.serve(app.fetch);
