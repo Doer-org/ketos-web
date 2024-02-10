c04faecec4b717c733eb89eb699448c9379b417c
miso-devel <kengo279@icloud.com>
2024-02-10 14:54:15 +0900

    cloudstorage to s3

---
+++ server/s3.ts
@@ -0,0 +1,27 @@
+import { GetObjectCommand, PutObjectCommand, S3 } from "./deps.ts";
+import { SECRET } from "./secret.ts";
+
+const client = new S3({
+  region: "ap-northeast-1",
+  credentials: {
+    accessKeyId: SECRET.ACCESS_KEY,
+    secretAccessKey: SECRET.SECRET_KEY,
+  },
+});
+
+export const getFileByS3 = async (id: string): Promise<ArrayBuffer> => {
+  const input = { Bucket: SECRET.BUCKET_NAME, Key: id };
+  const command = new GetObjectCommand(input);
+  const res = await client.send(command);
+  return await res.Body.transformToByteArray();
+};
+
+export const uploadFileByS3 = async (id: string, file: File) => {
+  const input = {
+    Bucket: SECRET.BUCKET_NAME,
+    Key: id,
+    Body: await file.arrayBuffer(),
+  };
+  const command = new PutObjectCommand(input);
+  await client.send(command);
+};
