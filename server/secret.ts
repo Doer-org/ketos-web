0bdbff8dd42d72c98fa227d783ce26c3f63162d7
miso-devel <kengo279@icloud.com>
2024-02-10 14:53:41 +0900

    環境変数整える

--- server/secret.ts
+++ server/secret.ts
@@ -2,5 +2,6 @@ import "https://deno.land/std@0.214.0/dotenv/load.ts";
 
 export const SECRET = {
   BUCKET_NAME: Deno.env.get("BUCKET_NAME")!,
-  TOKEN: Deno.env.get("TOKEN")!,
+  ACCESS_KEY: Deno.env.get("ACCESS_KEY")!,
+  SECRET_KEY: Deno.env.get("SECRET_KEY")!,
 };
