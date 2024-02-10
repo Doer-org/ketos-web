7d3d5f1a12e186392f4e399400a3da90dd8de5e7
miso-devel <kengo279@icloud.com>
2024-02-10 19:53:00 +0900

    Logo調整

--- client/app/components/Logo/index.tsx
+++ client/app/components/Logo/index.tsx
@@ -1,137 +1,139 @@
 export const Logo = () => {
   return (
-    <svg
-      width='261'
-      height='208'
-      viewBox='0 0 261 208'
-      fill='none'
-      className='w-20 h-20'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        d='M7.00005 134.66C21.9622 107.321 43.1585 114.725 51.8864 121.844V134.66C36.9243 155.164 15.728 143.203 7.00005 134.66Z'
-        fill='#001E57'
-        stroke='#001E57'
-        strokeWidth='10'
-      />
-      <path
-        d='M253.875 132.705C238.913 108.119 217.716 114.778 208.989 121.18V132.705C223.951 151.145 245.147 140.388 253.875 132.705Z'
-        fill='#001E57'
-        stroke='#001E57'
-        strokeWidth='10'
-      />
-      <g filter='url(#filter0_d_4_140)'>
+    <div className='rounded-full bg-white p-3'>
+      <svg
+        width='261'
+        height='208'
+        viewBox='0 0 261 208'
+        fill='none'
+        className='w-20 h-20'
+        xmlns='http://www.w3.org/2000/svg'
+      >
         <path
-          d='M223.951 99.8838C223.951 152.925 182.083 195.924 130.437 195.924C78.7915 195.924 36.9242 152.925 36.9242 99.8838C36.9242 42.2577 78.7915 0 130.437 0C182.083 0 223.951 34.5744 223.951 99.8838Z'
+          d='M7.00005 134.66C21.9622 107.321 43.1585 114.725 51.8864 121.844V134.66C36.9243 155.164 15.728 143.203 7.00005 134.66Z'
           fill='#001E57'
-        />
-        <path
-          d='M218.951 99.8838C218.951 150.29 179.197 190.924 130.437 190.924C81.6777 190.924 41.9242 150.29 41.9242 99.8838C41.9242 44.7867 81.7797 5 130.437 5C155.141 5 177.186 13.2543 193.038 28.9985C208.865 44.7175 218.951 68.3326 218.951 99.8838Z'
           stroke='#001E57'
           strokeWidth='10'
         />
-      </g>
-      <g filter='url(#filter1_d_4_140)'>
         <path
-          d='M224.033 103.96C224.033 157.001 182.166 200 130.52 200C78.8743 200 37.007 157.001 37.007 103.96C37.007 103.96 71.3932 103.96 123.039 103.96C174.685 103.96 224.033 103.96 224.033 103.96Z'
-          fill='#E7F3FF'
-        />
-        <path
-          d='M218.902 108.96C216.377 157.026 177.657 195 130.52 195C83.3836 195 44.6635 157.026 42.138 108.96L43.1868 108.96L60.6558 108.96L123.039 108.96L192.904 108.96L215.516 108.96L218.902 108.96Z'
+          d='M253.875 132.705C238.913 108.119 217.716 114.778 208.989 121.18V132.705C223.951 151.145 245.147 140.388 253.875 132.705Z'
+          fill='#001E57'
           stroke='#001E57'
           strokeWidth='10'
         />
-      </g>
-      <path d='M68 168L66.8484 103.125' stroke='#001E57' strokeWidth='10' />
-      <path d='M194 169L194.026 103.725' stroke='#001E57' strokeWidth='10' />
-      <path d='M89.2916 188.24V103.725' stroke='#001E57' strokeWidth='10' />
-      <path
-        d='M111.735 195.924L111.735 103.725'
-        stroke='#001E57'
-        strokeWidth='10'
-      />
-      <path
-        d='M149.14 195.924L149.14 103.725'
-        stroke='#001E57'
-        strokeWidth='10'
-      />
-      <path
-        d='M171.583 188.24L171.583 103.725'
-        stroke='#001E57'
-        strokeWidth='10'
-      />
-      <defs>
-        <filter
-          id='filter0_d_4_140'
-          x='32.9242'
-          y='0'
-          width='195.026'
-          height='203.924'
-          filterUnits='userSpaceOnUse'
-          colorInterpolationFilters='sRGB'
-        >
-          <feFlood floodOpacity='0' result='BackgroundImageFix' />
-          <feColorMatrix
-            in='SourceAlpha'
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-            result='hardAlpha'
+        <g filter='url(#filter0_d_4_140)'>
+          <path
+            d='M223.951 99.8838C223.951 152.925 182.083 195.924 130.437 195.924C78.7915 195.924 36.9242 152.925 36.9242 99.8838C36.9242 42.2577 78.7915 0 130.437 0C182.083 0 223.951 34.5744 223.951 99.8838Z'
+            fill='#001E57'
           />
-          <feOffset dy='4' />
-          <feGaussianBlur stdDeviation='2' />
-          <feComposite in2='hardAlpha' operator='out' />
-          <feColorMatrix
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+          <path
+            d='M218.951 99.8838C218.951 150.29 179.197 190.924 130.437 190.924C81.6777 190.924 41.9242 150.29 41.9242 99.8838C41.9242 44.7867 81.7797 5 130.437 5C155.141 5 177.186 13.2543 193.038 28.9985C208.865 44.7175 218.951 68.3326 218.951 99.8838Z'
+            stroke='#001E57'
+            strokeWidth='10'
           />
-          <feBlend
-            mode='normal'
-            in2='BackgroundImageFix'
-            result='effect1_dropShadow_4_140'
+        </g>
+        <g filter='url(#filter1_d_4_140)'>
+          <path
+            d='M224.033 103.96C224.033 157.001 182.166 200 130.52 200C78.8743 200 37.007 157.001 37.007 103.96C37.007 103.96 71.3932 103.96 123.039 103.96C174.685 103.96 224.033 103.96 224.033 103.96Z'
+            fill='#E7F3FF'
           />
-          <feBlend
-            mode='normal'
-            in='SourceGraphic'
-            in2='effect1_dropShadow_4_140'
-            result='shape'
+          <path
+            d='M218.902 108.96C216.377 157.026 177.657 195 130.52 195C83.3836 195 44.6635 157.026 42.138 108.96L43.1868 108.96L60.6558 108.96L123.039 108.96L192.904 108.96L215.516 108.96L218.902 108.96Z'
+            stroke='#001E57'
+            strokeWidth='10'
           />
-        </filter>
-        <filter
-          id='filter1_d_4_140'
-          x='33.007'
-          y='103.96'
-          width='195.026'
-          height='104.04'
-          filterUnits='userSpaceOnUse'
-          colorInterpolationFilters='sRGB'
-        >
-          <feFlood floodOpacity='0' result='BackgroundImageFix' />
-          <feColorMatrix
-            in='SourceAlpha'
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
-            result='hardAlpha'
-          />
-          <feOffset dy='4' />
-          <feGaussianBlur stdDeviation='2' />
-          <feComposite in2='hardAlpha' operator='out' />
-          <feColorMatrix
-            type='matrix'
-            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
-          />
-          <feBlend
-            mode='normal'
-            in2='BackgroundImageFix'
-            result='effect1_dropShadow_4_140'
-          />
-          <feBlend
-            mode='normal'
-            in='SourceGraphic'
-            in2='effect1_dropShadow_4_140'
-            result='shape'
-          />
-        </filter>
-      </defs>
-    </svg>
+        </g>
+        <path d='M68 168L66.8484 103.125' stroke='#001E57' strokeWidth='10' />
+        <path d='M194 169L194.026 103.725' stroke='#001E57' strokeWidth='10' />
+        <path d='M89.2916 188.24V103.725' stroke='#001E57' strokeWidth='10' />
+        <path
+          d='M111.735 195.924L111.735 103.725'
+          stroke='#001E57'
+          strokeWidth='10'
+        />
+        <path
+          d='M149.14 195.924L149.14 103.725'
+          stroke='#001E57'
+          strokeWidth='10'
+        />
+        <path
+          d='M171.583 188.24L171.583 103.725'
+          stroke='#001E57'
+          strokeWidth='10'
+        />
+        <defs>
+          <filter
+            id='filter0_d_4_140'
+            x='32.9242'
+            y='0'
+            width='195.026'
+            height='203.924'
+            filterUnits='userSpaceOnUse'
+            colorInterpolationFilters='sRGB'
+          >
+            <feFlood floodOpacity='0' result='BackgroundImageFix' />
+            <feColorMatrix
+              in='SourceAlpha'
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+              result='hardAlpha'
+            />
+            <feOffset dy='4' />
+            <feGaussianBlur stdDeviation='2' />
+            <feComposite in2='hardAlpha' operator='out' />
+            <feColorMatrix
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+            />
+            <feBlend
+              mode='normal'
+              in2='BackgroundImageFix'
+              result='effect1_dropShadow_4_140'
+            />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='effect1_dropShadow_4_140'
+              result='shape'
+            />
+          </filter>
+          <filter
+            id='filter1_d_4_140'
+            x='33.007'
+            y='103.96'
+            width='195.026'
+            height='104.04'
+            filterUnits='userSpaceOnUse'
+            colorInterpolationFilters='sRGB'
+          >
+            <feFlood floodOpacity='0' result='BackgroundImageFix' />
+            <feColorMatrix
+              in='SourceAlpha'
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+              result='hardAlpha'
+            />
+            <feOffset dy='4' />
+            <feGaussianBlur stdDeviation='2' />
+            <feComposite in2='hardAlpha' operator='out' />
+            <feColorMatrix
+              type='matrix'
+              values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+            />
+            <feBlend
+              mode='normal'
+              in2='BackgroundImageFix'
+              result='effect1_dropShadow_4_140'
+            />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='effect1_dropShadow_4_140'
+              result='shape'
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
   );
 };
