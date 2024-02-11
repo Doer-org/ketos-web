d59b22578784a4c87f59b6cdafe229e6f9ba1813
miso-devel <kengo279@icloud.com>
2024-02-11 13:10:59 +0900

    詳細ページをちゃんとする

--- client/app/routes/info.$id.tsx
+++ client/app/routes/info.$id.tsx
@@ -1,7 +1,7 @@
-import type { MetaFunction } from '@remix-run/cloudflare';
-import { useMatches } from '@remix-run/react';
-
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
+import { json, useLoaderData } from '@remix-run/react';
 import { Wrapper } from '~/components/Wrapper';
+import { cdate } from 'cdate';
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,11 +10,55 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+type FileInfo = {
+  id: string;
+  port: string;
+  createdAt: string;
+  size: number;
+};
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const res = (await fetch(
+    `https://ketos-server.doer-app.com/file_info/${params.id}`
+  )
+    .then(async (info) => await info.json())
+    .catch(() => null)) as FileInfo | null;
+
+  return json({ id: params.id, value: res });
+};
+
+type InfoProps = { label: string; value?: string | number };
+const Info = ({ label, value }: InfoProps) => {
+  return (
+    <div className='flex gap-3 items-center'>
+      <p className='font-bold p-1 bg-white w-1/3 text-gray-900 rounded-md'>
+        {label}
+      </p>
+      <p>{value ?? 'NONE'}</p>
+    </div>
+  );
+};
+
 export default function InfoDetail() {
-  const matches = useMatches()[1];
+  const { id, value } = useLoaderData<typeof loader>();
+  const now = cdate(value?.createdAt).utcOffset(+9);
   return (
     <Wrapper>
-      <h1 className='text-2xl font-bold'>ID:{matches.params.id}</h1>
+      <div>
+        <h1 className='text-2xl font-bold'>ID:{id}</h1>
+        {value ? (
+          <div className='flex gap-3 flex-col mt-10'>
+            <Info label='Port' value={value.port} />
+            <Info label='Size' value={value.size} />
+            <Info
+              label='Created At'
+              value={now.format('YYYY/MM/DD HH:mm:ss')}
+            />
+          </div>
+        ) : (
+          <p>Not Found Data</p>
+        )}
+      </div>
     </Wrapper>
   );
 }
